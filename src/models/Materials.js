2e7e1d592d08f60126a9d9dd3cfc7678fd6a6dc0
Ubuntu <vipwhy12@naver.com>
2023-01-06 23:12:28 +0000

    test

--- src/models/Materials.js
+++ src/models/Materials.js
@@ -1,46 +1,40 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const MaterialsSchema = new mongoose.Schema({
-//   materialId:{ 
-//     type : String,
-//     required: true, 
-//     unique: true
-//   }, 
-  
-//   name:{
-//     type : String,
-//     required : true
-//   },
+const MaterialsSchema = new mongoose.Schema({
+  name:{
+    type : String,
+    required : true
+  },
 
-//   type:{
-//     type : String,
-//     required: true
-//   },
+  type:{
+    type : String,
+    required: true
+  },
 
-//   content:{
-//     type : mongoose.Schema.Types.ObjectId, 
-//     required: true, 
-//     ref:"ClassMaterials"
-//   },
+  content:{
+    type : mongoose.Schema.Types.ObjectId, 
+    required: true, 
+    ref:"ClassMaterials"
+  },
 
-//   createAt:{
-//     type : date,
-//     required: true
-//   },
+  createAt:{
+    type : date,
+    required: true
+  },
 
-//   users : {
-//     type : String,
-//     require : true,
-//     ref : "Users"
-//   }, 
+  users : {
+    type : String,
+    require : true,
+    ref : "Users"
+  }, 
 
-//   classMaterial : {
-//     type : mongoose.Schema.Types.ObjectId, 
-//     require : true,
-//     ref : "ClassMaterials"
-//   }
-// });
+  classMaterial : {
+    type : mongoose.Schema.Types.ObjectId, 
+    require : true,
+    ref : "ClassMaterials"
+  }
+});
 
 
-// const Material = mongoose.model('Materials', MaterialsSchema);
-// module.exports = Material;
+const Material = mongoose.model('Materials', MaterialsSchema);
+module.exports = Material;
