33f49804b999a66fcef4d2427e2b76b6cd0f7fb5
yoojinLiz <yj.oh.biz@gmail.com>
2023-01-07 16:40:15 +0900

    urlencode middleware 추가

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,17 +1,9 @@
 import express from "express";
-import { join } from "../controllers/userController.js";
+import { postLogin, postJoin } from "../controllers/userController.js";
 
 const globalRouter = express.Router();
 
-const handelHome = (req, res) => {
-  res.send("home");
-}
-
-const handelJoin = (req, res) => {
-  res.send("Join");
-}
-
-globalRouter.get("/", handelHome); 
-globalRouter.get("/join", handelJoin);
+globalRouter.post("/login", postLogin);
+globalRouter.post("/join", postJoin);
 
 export default globalRouter;
--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ import morgan from "morgan"
 
 import globalRouter from "./routers/globalRouter.js";
 import userRouter from "./routers/userRouter.js";
-import classRouter from "./routers/classRouter.js";
-import liveRouter from "./routers/liveRouter.js";
-import materialRouter from "./routers/materialRouter.js";
-import userInfoRouter from "./routers/userInfoRouter.js";
+// import classRouter from "./routers/classRouter.js";
+// import liveRouter from "./routers/liveRouter.js";
+// import materialRouter from "./routers/materialRouter.js";
+// import userInfoRouter from "./routers/userInfoRouter.js";
 
 const logger = morgan("dev");
 const PORT = 4000; // express application을 바로 사용할 수 있도록 return
@@ -16,13 +16,18 @@ const app = express();
 // 로깅 
 app.use(logger);
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.text()); 
+app.use(express.json()); //string을 받아서 json으로 바꿔주는 middleware (JSON.parse를 해준다고 생각하면 됨  )
+
+
 // 라우터
 app.use("/", globalRouter);
 app.use("/user", userRouter);
-app.use("/class", classRouter);
-app.use("/live", liveRouter);
-app.use("/material", materialRouter);
-app.use("/userinfo", userInfoRouter);
+// app.use("/class", classRouter);
+// app.use("/live", liveRouter);
+// app.use("/material", materialRouter);
+// app.use("/userinfo", userInfoRouter);
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀 `);
 app.listen(PORT, handleListening);
