bdc021a1b6976787a4cc03204d684050f35e16fb
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-01-15 02:41:06 +0900

    update mongoDB

--- src/db.js
+++ src/db.js
@@ -1,11 +1,30 @@
+// import mongoose from "mongoose";
+
+// // console.log(process.platform);
+// // let dbAddr = "10.0.10.84"
+// // if (process.platform != "linux") {
+// // 	dbAddr = "127.0.0.1"
+// // }
+// // console.log(dbAddr);
+
+// // mongoose.connect(`mongodb://${dbAddr}:27017`,{
+// // 	useNewUrlParser:true, 
+// // 	useUnifiedTopology:true, 
+// // 	} //두번째 인자 부분은 아래에서 설명
+// // );
+
+// const db = mongoose.connection;
+// const handleOpen = () => console.log("✅ Connected to DB");
+// const handleError = (error) => console.log("❌ DB Error", error) 
+// db.once("open", handleOpen); //open 이벤트가 발생 시 handleOpen 실행 
+// db.on("error", handleError); //error 이벤트가 발생할 때마다 handleError 실행 ); 
+
+
+
 import mongoose from "mongoose";
 
-// console.log(process.platform);
-let dbAddr = "10.0.10.84"
-if (process.platform != "linux") {
-	dbAddr = "127.0.0.1"
-}
-mongoose.connect(`mongodb://${dbAddr}:27017/kidsquiz`,{
+// mongoose.connect("mongodb://10.0.10.84:27017",{
+mongoose.connect("mongodb://127.0.0.1:27017/kidsquiz",{
 	useNewUrlParser:true, 
 	useUnifiedTopology:true, 
 	} //두번째 인자 부분은 아래에서 설명
@@ -15,4 +34,3 @@ const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = (error) => console.log("❌ DB Error", error) 
 db.once("open", handleOpen); //open 이벤트가 발생 시 handleOpen 실행 
 db.on("error", handleError); //error 이벤트가 발생할 때마다 handleError 실행 ); 
-
--- src/routers/classMaterialRouter.js
+++ src/routers/classMaterialRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import { checkToken } from "../middleware.js";
 import { getClassMaterial } from "../controllers/materialController.js";
+import materialRouter from "./materialRouter.js";
 
 const classMaterialRouter = express.Router();
 
