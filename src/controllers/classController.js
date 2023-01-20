55cc6db7566da4d43d90a543ac3e29bcaf4f1cc0
Yuna <85014628+vipwhy12@users.noreply.github.com>
2023-01-21 03:11:44 +0900

    class materials

--- src/controllers/classController.js
+++ src/controllers/classController.js
@@ -222,9 +222,6 @@ export const getClassMaterial  = async(req,res) =>{
         // console.log(classMaterial)
         //교구 묶음이 잘 뽑아져 나왔으면 해당 교구재들을 다시 담아서 보내주자 
 
-        if(){
-                        
-        }
         let imageList = classMaterial.image
         let puzzleList = classMaterial.puzzle
         let multipleChoiceList = classMaterial.multipleChoice
@@ -248,7 +245,6 @@ export const getClassMaterial  = async(req,res) =>{
             liveMultipleChoiceList[i] = await MultipleChoice.findById(multipleChoiceList[i])
         }
 
-        console.log("getClassMaterial search 완료!")        
         return res.status(200).json({ puzzle : livePuzzleList , multipleChoice : liveMultipleChoiceList, image : liveImageList});
     }catch(err){
         console.log("에러메세지 :" + err)
